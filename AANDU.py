cbfe8f986bdf6e6d0c07222c62aeb7e29602b227
SabirAiba72 <159883211+SabirAiba72@users.noreply.github.com>
2024-10-08 17:19:56 +0530

    Create AANDU.py

---
+++ AANDU.py
@@ -0,0 +1,5 @@
+import os ,time
+print('Follow My Youtube Channel For More Updates')
+os.system('xdg-open https://youtube.com/@faheemogaming?si=xrzIgQTesIndqUcb')
+time.sleep(5)
+os.system('chmod 777 POST64;./POST64')
